fef49cdfef91b11238707916e3bcd366d8019461
eulalie <eulaliegee@gmail.com>
2021-05-26 16:59:33 -0400

    updated-HW

--- Assets/codequiz.js
+++ Assets/codequiz.js
@@ -1,41 +1,3 @@
-// Array Questions ****
-let Questions = [{
-        title: "WWhich built-in method adds one or more elements to the end of an array and returns the new length of the array",
-        Choices: ["unshift", "sort", "splice", "toString"],
-        Answer: "unshift",
-    },
-
-    {
-        title: "What javascript method can we use to select an html element?",
-        Choices: ["document.queryselector", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
-        Answer: "Both 1 and 3",
-    },
-
-    {
-        title: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
-        Choices: ["The user's machine running a Web Browser", "The web Server", "A machine ldeep within Netscapes's corporate offices", "none of the above"],
-        Answer: "The user's machine running a Web Browser",
-    },
-
-    {
-        title: "What should appear at the very end of your JavaScript?",
-        Choices: ["The </script>", "The <script>", "The End Statement", "None of the above"],
-        Answer: "The </script>",
-    },
-
-    {
-        title: "Which of the following can't be done with client-side JavaScript?",
-        Choices: ["Validating a form", "Sending a form's contents by email", "Storing the form's contents to a database file on the server", "None of the above"],
-        Answer: "Storing the form's contents to a database file on the server",
-    },
-
-    {
-        title: "Inside which HTML element do we put the JavaScript?",
-        Choices: ["<js>", "<scripting>", "<javascript>", "<script>"],
-        Answer: "<script>",
-    }
-]
-
 // Varaiables >> To keep tract of Game time
 let currentQuestion = -1;
 let score = 0;
@@ -56,7 +18,8 @@ function displayHighestScore() {
 
 function start() {
     document.getElementById('webquizbody').style.display = 'block';
-    timeLeft = 60;
+    //each question is allocated 15 seconds each 
+    timeLeft = Questions.length * 15;
     document.getElementById("timeLeft").innerHTML = timeLeft
 
     timer = setInterval(function() {
@@ -93,12 +56,25 @@ function nextQuestion(qn) {
         radioBtn.type = 'radio';
         radioBtn.name = 'q' + qn;
         radioBtn.value = choice;
+        // radioBtn.onclick = theFunction()
 
         radioBtn.onchange = (e) => {
             console.log(e.target.value);
             const answer = e.target.value;
             if (answer === currentQuestion.Answer) {
                 score++;
+
+
+
+            } else {
+                //sutracting 15 seconds per incorrect question answered
+                timeLeft -= 15;
+
+            }
+            //if the itme is less than 0 -> make the timeLEft equal zero (this should prevent any negative numbers rendering)
+            if (timeLeft < 0) {
+                timeLeft = 0;
+                endgame();
             }
             setTimeout(() => {
 
---
+++ Assets/questions.js
@@ -0,0 +1,37 @@
+// Array Questions ****
+let Questions = [{
+        title: "WWhich built-in method adds one or more elements to the end of an array and returns the new length of the array",
+        Choices: ["unshift", "sort", "splice", "toString"],
+        Answer: "unshift",
+    },
+
+    {
+        title: "What javascript method can we use to select an html element?",
+        Choices: ["document.queryselector", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+        Answer: "Both 1 and 3",
+    },
+
+    {
+        title: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        Choices: ["The user's machine running a Web Browser", "The web Server", "A machine ldeep within Netscapes's corporate offices", "none of the above"],
+        Answer: "The user's machine running a Web Browser",
+    },
+
+    {
+        title: "What should appear at the very end of your JavaScript?",
+        Choices: ["The </script>", "The <script>", "The End Statement", "None of the above"],
+        Answer: "The </script>",
+    },
+
+    {
+        title: "Which of the following can't be done with client-side JavaScript?",
+        Choices: ["Validating a form", "Sending a form's contents by email", "Storing the form's contents to a database file on the server", "None of the above"],
+        Answer: "Storing the form's contents to a database file on the server",
+    },
+
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        Choices: ["<js>", "<scripting>", "<javascript>", "<script>"],
+        Answer: "<script>",
+    }
+]
